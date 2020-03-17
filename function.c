89d1df689f8cd03f2b31ba528f63b272b4036fa2
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-03-17 20:30:26 +0000

    printf

--- function.c
+++ function.c
@@ -2,6 +2,34 @@
 #include <stdlib.h>
 #include "holberton.h"
 /**
+ * b_bin - fun
+ * @c: c
+ * Return: binary
+ */
+int b_bin(va_list c)
+{
+int i, j, n, r;
+int l[32];
+int s = 0;
+n = va_arg(c, int);
+for (i = 0; n != 0; i++)
+{ 
+r = n % 2;
+l[i] = r;
+n = (n - r) / 2;
+}
+for (j = 0; j < i; j++)
+{
+if (l[i])
+{
+_putchar(l[i] + '0');
+s++;
+}
+}
+return (s);
+}
+
+/**
  * i_int - fn print integer d_or_i
  *@c : var
  *Return : Always 0
