898f8f9e35596940cae8249678c11028fb324184
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-03-17 21:52:06 +0000

    bin

--- b_bin.c
+++ b_bin.c
@@ -12,13 +12,12 @@ int i, j, n, r, l[32];
 int s = 0;
 i = 0;
 n = va_arg(c, unsigned int);
-do
-{
+do {
 r = n % 2;
 l[i] = r;
 n = (n - r) / 2;
 i++;
-}while (n != 0);
+} while (n != 0);
 
 j = i - 1;
 while (j >= 0)
