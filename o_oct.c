e8fa1e7eb79deed06ea68d926530a755114a635d
mahdibenzouitina <ben.zouitina.mahdi97@gmail.com>
2020-03-17 22:55:46 +0000

    fix

--- o_oct.c
+++ o_oct.c
@@ -9,19 +9,20 @@
  */
 int o_oct(va_list c)
 {
-int n, i, j, s;
-int l[1000];
+	int n, i, j, s;
+	int l[1000];
+
 n = va_arg(c, int);
-while (n != 0)
-{
-l[i++] = n % 8;
-n  = n / 8;
-}
-s = 0;
-for (j = i - 1; l[j] != '\0'; j--)
-{
-_putchar(l[j] + '0');
-s++;
-}
-return (s);
+	while (n != 0)
+	{
+		l[i++] = n % 8;
+		n  = n / 8;
+	}
+	s = 0;
+	for (j = i - 1; l[j] != '\0'; j--)
+	{
+		_putchar(l[j] + '0');
+		s++;
+	}
+	return (s);
 }
