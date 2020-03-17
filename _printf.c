b1378efbea2e25ec9e77e3643d012ab8f1b61d4d
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-03-17 00:13:27 +0000

    printf

--- _printf.c
+++ _printf.c
@@ -8,15 +8,27 @@
    */
 int _printf(const char *format, ...)
 {
-int i;
+
+  int s, i, f;
+int (*f1)(va_list, char);
 va_list ap;
+
+
 va_start(ap, format);
 if (format == NULL)
 return (-1);
-i=0;
+i = 0;
 while(format[i])
 {
-
+  if (get_op_func(format[i])
+    {    
+  s += get_op_func(ap)
+i+=2
+    }
+    else
+      if(format[i + 1] == '\0')
+	return (-1);
+ 
 }
 va_end(ap);
 return i;
--- holberton.h
+++ holberton.h
@@ -2,12 +2,19 @@
 #define HOLBERTON_H 
 #include <stdarg.h> 
 #include <stdlib.h> 
-/** 
+/**
 * struct printing - print types. 
 * @str:char types. 
-* @fn: function. 
-* 
+* @fn: function.
+*
 */
+typedef struct op
+{
+char *op;
+int (*f)(va_list p);
+} op_t;
 int _putchar(char c); 
+int c_char(va_list c);
+int s_string(va_list c);
 int _printf(const char *format, ...);  
 #endif 
