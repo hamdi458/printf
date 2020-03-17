5299c798e4205fcb31d967df2259dd34bd48650d
mahdibenzouitina <ben.zouitina.mahdi97@gmail.com>
2020-03-17 23:10:52 +0000

    fix

--- inte.c
+++ inte.c
@@ -2,17 +2,17 @@
 /**
  * print_number - print number.
  *@n : the number
- * Return: Always 0.
+ * Return: Always 00.
  */
 void print_number(int n)
 {
-unsigned int num = n;
-if (n < 0)
-{
-_putchar('-');
-num = -num;
-}
-if ((num / 10) > 0)
-print_number(num / 10);
-_putchar((num % 10) + '0');
+	unsigned int num = n;
+        if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	if ((num / 10) > 0)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
 }
