948dd6394f139329d0feaa0f9c6c1638069da785
mahdibenzouitina <ben.zouitina.mahdi97@gmail.com>
2020-03-17 23:07:57 +0000

    fix

--- r_rev.c
+++ r_rev.c
@@ -3,23 +3,23 @@
 /**
  * r_rev - fun
  * @r: var
- * Return: 0
+ * Return: 00
  */
 int r_rev(va_list c)
 {
-char *ch;
-int i = 0, j, s,x;
-s = 0;
-x = 0;
-ch = va_arg(c, char *);
-if (ch == NULL)
-ch = ")llun(";
-for (i = 0; ch[i] != '\0'; i++)
-x++;
-for (j = x - 1; j >= 0; j--)
-{
-_putchar(ch[j]);
-s++;
-}
-return (s);
+	char *ch;
+	int i = 0, j, s,x;
+	s = 0;
+	x = 0;
+	ch = va_arg(c, char *);
+	if (ch == NULL)
+		ch = ")llun(";
+	for (i = 0; ch[i] != '\0'; i++)
+		x++;
+	for (j = x - 1; j >= 0; j--)
+	{
+		_putchar(ch[j]);
+		s++;
+	}
+	return (s);
 }
