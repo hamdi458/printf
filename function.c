f661eaf85a9c280b1f8d0d8d048231be28eb0f36
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-03-17 20:59:43 +0000

    printf_bin

--- function.c
+++ function.c
@@ -10,20 +10,21 @@ int b_bin(va_list c)
 {
 unsigned int i, j, n, r, l[32];
 int s = 0;
+i = 0;
 n = va_arg(c, unsigned int);
-for (i = 0; n != 0; i++)
-{ 
+do 
+{
 r = n % 2;
 l[i] = r;
 n = (n - r) / 2;
+i++;
 }
-for (j = 0; j < i; j++)
+while (n != 0);
+
+for (j = i - 1; l[j]; j--)
 {
-if (l[i])
-{
-_putchar(l[i] + '0');
+_putchar(l[j] + '0');
 s++;
-}
 }
 return (s);
 }
