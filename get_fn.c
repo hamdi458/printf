8ecf580bd05acb8b3120b0de5c4737c259f9b16e
mahdibenzouitina <ben.zouitina.mahdi97@gmail.com>
2020-03-17 22:49:42 +0000

    fix

--- get_fn.c
+++ get_fn.c
@@ -8,24 +8,24 @@
  */
 int (*get_op_func(const char *c))(va_list ap)
 {
-int k;
-op_t ops[] = {
-{"c", c_char},
-{"s", s_string},
-{"i", i_int},
-{"d", i_int},
-{"b", b_bin},
-{"r", r_rev},
-{"R", R_rot},
-{"o", o_oct},
-{NULL, NULL}
-};
-k = 0;
-while (k <= 7)
-{
-if (*c == *(ops + k)->op)
-return ((ops + k)->f);
-k++;
-}
-return (NULL);
+	int k;
+	op_t ops[] = {
+		{"c", c_char},
+		{"s", s_string},
+		{"i", i_int},
+		{"d", i_int},
+		{"b", b_bin},
+		{"r", r_rev},
+		{"R", R_rot},
+		{"o", o_oct},
+		{NULL, NULL}
+	};
+	k = 0;
+	while (k <= 7)
+	{
+		if (*c == *(ops + k)->op)
+			return ((ops + k)->f);
+		k++;
+	}
+	return (NULL);
 }
