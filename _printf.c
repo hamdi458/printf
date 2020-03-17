0a758249f760c2821c71e566980ebf27b74f2a2b
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-03-17 02:20:03 +0000

    print

--- _printf.c
+++ _printf.c
@@ -11,7 +11,7 @@ int _printf(const char *format, ...)
 
 int s, i;
 va_list ap;
- int (*f)(va_list);
+int (*f)(va_list);
 va_start(ap, format);
 if (format == NULL)
 return (-1);
@@ -19,11 +19,37 @@ i = 0;
 s = 0;
 while(format[i])
 {    
-f = get_op_func(&format[i]);
- s = s + f(ap);
- i = i+ 1;
-if(format[i + 1] == '\0')
-return (-1);
+  if (format[i] != '%')
+    {
+      _putchar(format[i]);
+	i++;
+      s++;
+    }
+  else
+    break;
+}
+ while(format[i])
+   {
+     f = get_op_func(&format[i + 1]);
+     if(f == NULL && format[i] == '%')
+       {
+	 if(format[i + 1]=='\0')
+	   return (-1);
+	 if(format[i + 1]=='%')
+	   {	   
+	     _putchar('%');
+	     s++;
+	     i++;
+	   }
+
+}
+else
+if (f)
+{
+s += f(ap);
+i++;
+}
+i++;
 }
 va_end(ap);
 return (s);
--- function.c
+++ function.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "holberton.h"
+
 /**
    * c_char - fn
    * @args: var
